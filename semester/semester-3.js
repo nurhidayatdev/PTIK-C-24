a9175c053183784b6d5451c0580062006c23f436
Yaya Project <117073849+YayaProject@users.noreply.github.com>
2025-08-19 20:08:05 +0800

    Add files via upload

---
+++ semester/semester-3.js
@@ -0,0 +1,121 @@
+// 1
+const hari_1 = "Senin";
+const jam_1 = "I-III";
+const pukul_1 = "07.30-10.05";
+const ruangan_1 = "C2-P4";
+const matkul_1 = "Keamanan Komputer";
+const sks_1 = "3";
+
+// 2
+const hari_2 = "Senin";
+const jam_2 = "X-XI";
+const pukul_2 = "15.45-17.30";
+const ruangan_2 = "AE 107";
+const matkul_2 = "Profesi Kependidikan";
+const sks_2 = "2";
+
+// 3
+const hari_3 = "Selasa";
+const jam_3 = "III-V";
+const pukul_3 = "09.15-11.50";
+const ruangan_3 = "Lab ICT 3";
+const matkul_3 = "Inovasi Teknologi";
+const sks_3 = "2";
+
+// 4
+const hari_4 = "Selasa";
+const jam_4 = "V-VII";
+const pukul_4 = "11.00-12.40";
+const ruangan_4 = "C2-P5";
+const matkul_4 = "Strategi Pembelajaran";
+const sks_4 = "3";
+
+// 5
+const hari_5 = "Selasa";
+const jam_5 = "IX-XI";
+const pukul_5 = "14.50-17.30";
+const ruangan_5 = "AE 105";
+const matkul_5 = "Kecerdasan Buatan";
+const sks_5 = "3";
+
+// 6
+const hari_6 = "Rabu";
+const jam_6 = "IV-VI";
+const pukul_6 = "10.05-12.40";
+const ruangan_6 = "Lab Jaringan";
+const matkul_6 = "Jaringan Komputer";
+const sks_6 = "3";
+
+// 7
+const hari_7 = "Kamis";
+const jam_7 = "I-III";
+const pukul_7 = "07.30-10.05";
+const ruangan_7 = "AE 102";
+const matkul_7 = "Pemrograman Web";
+const sks_7 = "3";
+
+// 8
+const hari_8 = "Jumat";
+const jam_8 = "IX-XI";
+const pukul_8 = "14.50-17.30";
+const ruangan_8 = "AE 107";
+const matkul_8 = "Struktur Data";
+const sks_8 = "3";
+
+
+// Set innerText
+document.getElementById("hari_1").innerText = hari_1;
+document.getElementById("jam_1").innerText = jam_1;
+document.getElementById("pukul_1").innerText = pukul_1;
+document.getElementById("ruangan_1").innerText = ruangan_1;
+document.getElementById("matkul_1").innerText = matkul_1;
+document.getElementById("sks_1").innerText = sks_1;
+
+document.getElementById("hari_2").innerText = hari_2;
+document.getElementById("jam_2").innerText = jam_2;
+document.getElementById("pukul_2").innerText = pukul_2;
+document.getElementById("ruangan_2").innerText = ruangan_2;
+document.getElementById("matkul_2").innerText = matkul_2;
+document.getElementById("sks_2").innerText = sks_2;
+
+document.getElementById("hari_3").innerText = hari_3;
+document.getElementById("jam_3").innerText = jam_3;
+document.getElementById("pukul_3").innerText = pukul_3;
+document.getElementById("ruangan_3").innerText = ruangan_3;
+document.getElementById("matkul_3").innerText = matkul_3;
+document.getElementById("sks_3").innerText = sks_3;
+
+document.getElementById("hari_4").innerText = hari_4;
+document.getElementById("jam_4").innerText = jam_4;
+document.getElementById("pukul_4").innerText = pukul_4;
+document.getElementById("ruangan_4").innerText = ruangan_4;
+document.getElementById("matkul_4").innerText = matkul_4;
+document.getElementById("sks_4").innerText = sks_4;
+
+document.getElementById("hari_5").innerText = hari_5;
+document.getElementById("jam_5").innerText = jam_5;
+document.getElementById("pukul_5").innerText = pukul_5;
+document.getElementById("ruangan_5").innerText = ruangan_5;
+document.getElementById("matkul_5").innerText = matkul_5;
+document.getElementById("sks_5").innerText = sks_5;
+
+document.getElementById("hari_6").innerText = hari_6;
+document.getElementById("jam_6").innerText = jam_6;
+document.getElementById("pukul_6").innerText = pukul_6;
+document.getElementById("ruangan_6").innerText = ruangan_6;
+document.getElementById("matkul_6").innerText = matkul_6;
+document.getElementById("sks_6").innerText = sks_6;
+
+document.getElementById("hari_7").innerText = hari_7;
+document.getElementById("jam_7").innerText = jam_7;
+document.getElementById("pukul_7").innerText = pukul_7;
+document.getElementById("ruangan_7").innerText = ruangan_7;
+document.getElementById("matkul_7").innerText = matkul_7;
+document.getElementById("sks_7").innerText = sks_7;
+
+document.getElementById("hari_8").innerText = hari_8;
+document.getElementById("jam_8").innerText = jam_8;
+document.getElementById("pukul_8").innerText = pukul_8;
+document.getElementById("ruangan_8").innerText = ruangan_8;
+document.getElementById("matkul_8").innerText = matkul_8;
+document.getElementById("sks_8").innerText = sks_8;
