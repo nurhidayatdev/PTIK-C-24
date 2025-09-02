71e6949e3a393156a5b41d6caf89dec0e2148c54
Nur Hidayat <yayagm0712@gmail.com>
2025-09-02 13:25:23 +0800

    update land page, daftar tugas

---
+++ daftar-tugas/daftar-tugas.js
@@ -0,0 +1,72 @@
+// Keamanan Komputer
+const tanggal_1 = "";
+const jam_1 = "";
+const deskripsi_1 = "";
+
+// Profesi Kependidikan
+const tanggal_2 = "";
+const jam_2 = "";
+const deskripsi_2 = "";
+
+// Inovasi Teknologi
+const tanggal_3 = "7 September 2025";
+const jam_3 = "";
+const deskripsi_3 = "Menetukan Judul Proposal PKM";
+
+// Strategi Pembelajaran
+const tanggal_4 = "";
+const jam_4 = "";
+const deskripsi_4 = "";
+
+// Kecerdasan Buatan
+const tanggal_5 = "";
+const jam_5 = "";
+const deskripsi_5 = "";
+
+// Jaringan Komputer
+const tanggal_6 = "";
+const jam_6 = "";
+const deskripsi_6 = "";
+
+// Pemrograman Web
+const tanggal_7 = "";
+const jam_7 = "";
+const deskripsi_7 = "";
+
+// Struktur Data
+const tanggal_8 = "5 September 2025";
+const jam_8 = "";
+const deskripsi_8 = "Laporan Modul 1";
+
+// Kode DOM
+document.getElementById("tanggal_1").innerText = tanggal_1;
+document.getElementById("jam_1").innerText = jam_1;
+document.getElementById("deskripsi_1").innerText = deskripsi_1;
+
+document.getElementById("tanggal_2").innerText = tanggal_2;
+document.getElementById("jam_2").innerText = jam_2;
+document.getElementById("deskripsi_2").innerText = deskripsi_2;
+
+document.getElementById("tanggal_3").innerText = tanggal_3;
+document.getElementById("jam_3").innerText = jam_3;
+document.getElementById("deskripsi_3").innerText = deskripsi_3;
+
+document.getElementById("tanggal_4").innerText = tanggal_4;
+document.getElementById("jam_4").innerText = jam_4;
+document.getElementById("deskripsi_4").innerText = deskripsi_4;
+
+document.getElementById("tanggal_5").innerText = tanggal_5;
+document.getElementById("jam_5").innerText = jam_5;
+document.getElementById("deskripsi_5").innerText = deskripsi_5;
+
+document.getElementById("tanggal_6").innerText = tanggal_6;
+document.getElementById("jam_6").innerText = jam_6;
+document.getElementById("deskripsi_6").innerText = deskripsi_6;
+
+document.getElementById("tanggal_7").innerText = tanggal_7;
+document.getElementById("jam_7").innerText = jam_7;
+document.getElementById("deskripsi_7").innerText = deskripsi_7;
+
+document.getElementById("tanggal_8").innerText = tanggal_8;
+document.getElementById("jam_8").innerText = jam_8;
+document.getElementById("deskripsi_8").innerText = deskripsi_8;
