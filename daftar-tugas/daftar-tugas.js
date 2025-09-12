cc333be836dedd7c60ef2ff9002f6ce5fa0e7818
Nur Hidayat <yayagm0712@gmail.com>
2025-09-12 15:05:21 +0800

    update width

--- daftar-tugas/daftar-tugas.js
+++ daftar-tugas/daftar-tugas.js
@@ -10,7 +10,7 @@ const jam_2 = "";
 const deskripsi_2 = "";
 
 // Inovasi Teknologi
-const tanggal_3 = "15 September 2025";
+const tanggal_3 = "15-09-2025";
 const jam_3 = "22.00";
 const deskripsi_3 = "Resume Materi & Pendahuluan Proposal PKM";
 
@@ -20,23 +20,23 @@ const jam_4 = "";
 const deskripsi_4 = "";
 
 // Kecerdasan Buatan
-const tanggal_5 = "17 September 2025";
+const tanggal_5 = "17-09-2025";
 const jam_5 = "14.50";
 const deskripsi_5 = "Tugas Kelompok: Pendekatan Representasi Pengetahuan";
 
 // Jaringan Komputer
-const tanggal_6 = "10 September 2025";
+const tanggal_6 = "10-09-2025";
 const jam_6 = "10.05";
 const deskripsi_6 = "Summary Protokol Jaringan";
 
 // Pemrograman Web
-const tanggal_7 = "10 September 2025";
+const tanggal_7 = "10-09-2025";
 const jam_7 = "24.00";
 const deskripsi_7 = "Praktikum 3: CSS";
 const link_7 = "https://docs.google.com/forms/d/e/1FAIpQLSdbpqdHFNEew-wefIaBbjz66ewZenODm9kyvzpRGqJ6tyWueA/viewform";
 
 // Struktur Data
-const tanggal_8 = "17 September 2025";
+const tanggal_8 = "17-09-2025";
 const jam_8 = "13.00";
 const deskripsi_8 = "Praktikum 2: Pointer";
 
