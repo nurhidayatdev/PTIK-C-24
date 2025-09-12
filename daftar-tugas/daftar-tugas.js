8ca7840e7b2dd487459824754a2c60656b8e53fb
Nur Hidayat <yayagm0712@gmail.com>
2025-09-12 11:44:13 +0800

    Update daftar-tugas.js

--- daftar-tugas/daftar-tugas.js
+++ daftar-tugas/daftar-tugas.js
@@ -11,7 +11,8 @@ const deskripsi_2 = "-";
 // Inovasi Teknologi
 const tanggal_3 = "15 September 2025";
 const jam_3 = "22.00";
-const deskripsi_3 = "resume dan pendahuluan";
+const deskripsi_3 = "- Resume Materi \n
+- Pendahuluan Proposal PKM";
 
 // Strategi Pembelajaran
 const tanggal_4 = "-";
@@ -21,22 +22,22 @@ const deskripsi_4 = "-";
 // Kecerdasan Buatan
 const tanggal_5 = "17 September 2025";
 const jam_5 = "jam masuk";
-const deskripsi_5 = "tugas kelompok";
+const deskripsi_5 = "Tugas Kelompok: Contoh Pendekatan Representasi Pengetahuan";
 
 // Jaringan Komputer
 const tanggal_6 = "10 September 2025";
 const jam_6 = "jamnya";
-const deskripsi_6 = "resume";
+const deskripsi_6 = "Summary Protokol Jaringan";
 
 // Pemrograman Web
 const tanggal_7 = "10 September 2025";
 const jam_7 = "24.00";
-const deskripsi_7 = "tugas dan praktikum";
+const deskripsi_7 = "Praktikum 3: CSS";
 
 // Struktur Data
 const tanggal_8 = "12 September 2025";
 const jam_8 = "12.00";
-const deskripsi_8 = "laprakknyaa bosss";
+const deskripsi_8 = "Praktikum 1: Array";
 
 // Kode DOM
 for (let i = 1; i <= 8; i++) {
